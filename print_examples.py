731bdfe5bc843f1e10fe388bcbb368f5277feaeb
vanshikashivnani <vanshikashivnani21@gmail.com>
2023-01-26 11:00:07 +0100

    this is the third session class example

---
+++ print_examples.py
@@ -0,0 +1,5 @@
+print('This is a text', end='|I want this at the end|\n')
+print('this is another')
+print('this',2+2, 'can', 'be', 'written', 'like', 'this')
+# sep indicates how you want to separate the items
+print('this',2+2, 'can', 'be', 'written', 'like', 'this', sep='!b ?! ')
